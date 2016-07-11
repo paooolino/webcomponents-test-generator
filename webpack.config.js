8f28b580e218d46eeaa53677061d57ed25f8e551
paooolino <paooolino@gmail.com>
2016-07-12 00:21:53 +0200

    setup

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: './dist',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    }
+};
